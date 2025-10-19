4634dd4fbf93a25456278c30fbe237a7ebc87343
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-10-19 22:30:56 +0000

    Update quiz.rb
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

--- quiz.rb
+++ quiz.rb
@@ -1,5 +1,11 @@
 require_relative "lib/Carriculum.rb"
+require_relative "lib/SecondCarriculum.rb"
 
+#####################################################################################################
+#                                      Information About Objects                                    #
+#####################################################################################################
+# This module helps seperate concerns about apples, bananas, and lemons.                            #
+#####################################################################################################
 def fruit
   Carriculum::LearnFruit.apple
   Carriculum::LearnFruit.banana
@@ -16,6 +22,27 @@ def drinks
   Carriculum::LearnDrinks.milk
   Carriculum::LearnDrinks.water
   Carriculum::LearnDrinks.cafe
+end
+
+#####################################################################################################
+#                                            Meta-Osint                                             #
+#####################################################################################################
+# Broad overview of data about information related to osint gathering.                              #
+#####################################################################################################
+def evaluate_malicious_users
+  SecondCarriculum::Metaosint.evaluate_users
+end
+
+def descrepancy_quotient
+  SecondCarriculum::Metaosint.descrapancies
+end
+
+def evaluate_yt_stats
+  SecondCarriculum::Metaosint.youtube_stats
+end
+
+def analyze_metadata
+  SecondCarriculum::Metaosint.dice_analyses
 end
 
 vegetables; fruit; drinks
